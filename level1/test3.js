daebd053e2e2a95dec388af2a14fa731849f4198
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-11-08 20:03:49 +0900

    음양수 더하기

---
+++ level1/test3.js
@@ -0,0 +1,15 @@
+// 음양수 더하기
+function solution(absolutes, signs) {
+  answer = 0
+  for (let i = 0; i < absolutes.length; i++) {
+    if (signs[i]) {
+      answer += +absolutes[i]
+    } else {
+      answer -= +absolutes[i]
+    }
+  }
+  return answer
+}
+const absolutes = [1, 2, 3]
+const signs = [false, false, true]
+console.log(solution(absolutes, signs))
