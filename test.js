806826315936bd84d07a36f21f2bd8f5bb4ae44e
Jungbam <108196588+Jungbam@users.noreply.github.com>
2022-11-21 20:29:37 +0900

    숫자열과 문자열과 영단어

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-// const a = [[2, 1], [2, 4], 3]
-// console.log(a[0]) // [2,1]
-// console.log(a[1][0]) // 2
-// console.log(a[1][1]) // 4
-// console.log(a[2]) // 3
-
-const a = (n = []) => {
-  const bb = [[]]
-  bb.push(n) // [ [], [ 1, 2 ] ]
-  bb[0].push(n) //  [ [ [ 1, 2 ] ] ]
-  console.log(bb)
+function solution(n) {
+  const num = [...String(n)]
+  // for문을 만들어서(인덱스가 있는 타입으로 바꾸겠다.=>
+  // 배열, String) n 값의 숫자를 돌아가면서 인식을 시킨다.
+  num.sort((a, b) => {
+    return b - a
+  })
+  const numValue = num.join('')
+  return Number(numValue)
 }
-a([1, 2])
+
+console.log(solution(118372))
